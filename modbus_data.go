e641e572020d4ca57d1b8fce9972e21c784e46f6
Dwayne Bradley <dwayne.bradley@gmail.com>
2023-03-01 09:46:13 -0500

    Rename signed-unsigned register variables

--- modbus_data.go
+++ modbus_data.go
@@ -11,12 +11,12 @@ func GetDeviceModbusData(mc *modbus.ModbusClient, registerConfigs []RegisterConf
 	var rva []RegisterValue
 	var err error
 
-	var reg16 uint16
-	var reg32 uint32
-	var reg64 uint64
-	var regs16 int16
-	var regs32 int32
-	var regs64 int64
+	var u16Register uint16
+	var u32Register uint32
+	var u64Register uint64
+	var s16Register int16
+	var s32Register int32
+	var s64Register int64
 
 	if mc == nil {
 		return nil, errors.New("Modbus client not connected")
@@ -29,47 +29,47 @@ func GetDeviceModbusData(mc *modbus.ModbusClient, registerConfigs []RegisterConf
 
 		switch register.Size {
 		case "UINT16":
-			reg16, err = mc.ReadRegister(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
+			u16Register, err = mc.ReadRegister(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
 			if err == nil {
-				rv.Value = fmt.Sprintf(register.Format, float32(reg16)*register.Multiplier)
+				rv.Value = fmt.Sprintf(register.Format, float32(u16Register)*register.Multiplier)
 			} else {
 				return nil, errors.New("Modbus error: " + err.Error())
 			}
 		case "SINT16":
-			reg16, err = mc.ReadRegister(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
+			u16Register, err = mc.ReadRegister(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
 			if err == nil {
-				regs16 = int16(reg16)
-				rv.Value = fmt.Sprintf(register.Format, float32(regs16)*register.Multiplier)
+				s16Register = int16(u16Register)
+				rv.Value = fmt.Sprintf(register.Format, float32(s16Register)*register.Multiplier)
 			} else {
 				return nil, errors.New("Modbus error: " + err.Error())
 			}
 		case "UINT32":
-			reg32, err = mc.ReadUint32(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
+			u32Register, err = mc.ReadUint32(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
 			if err == nil {
-				rv.Value = fmt.Sprintf(register.Format, float32(reg32)*register.Multiplier)
+				rv.Value = fmt.Sprintf(register.Format, float32(u32Register)*register.Multiplier)
 			} else {
 				return nil, errors.New("Modbus error: " + err.Error())
 			}
 		case "SINT32":
-			reg32, err = mc.ReadUint32(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
+			u32Register, err = mc.ReadUint32(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
 			if err == nil {
-				regs32 = int32(reg32)
-				rv.Value = fmt.Sprintf(register.Format, float32(regs32)*register.Multiplier)
+				s32Register = int32(u32Register)
+				rv.Value = fmt.Sprintf(register.Format, float32(s32Register)*register.Multiplier)
 			} else {
 				return nil, errors.New("Modbus error: " + err.Error())
 			}
 		case "UINT64":
-			reg64, err = mc.ReadUint64(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
+			u64Register, err = mc.ReadUint64(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
 			if err == nil {
-				rv.Value = fmt.Sprintf(register.Format, float64(reg64)*float64(register.Multiplier))
+				rv.Value = fmt.Sprintf(register.Format, float64(u64Register)*float64(register.Multiplier))
 			} else {
 				return nil, errors.New("Modbus error: " + err.Error())
 			}
 		case "SINT64":
-			reg64, err = mc.ReadUint64(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
+			u64Register, err = mc.ReadUint64(uint16(register.HoldingRegister), modbus.HOLDING_REGISTER)
 			if err == nil {
-				regs64 = int64(reg64)
-				rv.Value = fmt.Sprintf(register.Format, float64(regs64)*float64(register.Multiplier))
+				s64Register = int64(u64Register)
+				rv.Value = fmt.Sprintf(register.Format, float64(s64Register)*float64(register.Multiplier))
 			} else {
 				return nil, errors.New("Modbus error: " + err.Error())
 			}
